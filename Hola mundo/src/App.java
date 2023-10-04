5d17bd520068b8530e07df7fccbaaf9a0e86c707
Sebas J <juan.jaramillo0622@gmail.com>
2023-10-04 10:31:49 -0500

    cambio

---
+++ Hola mundo/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        char letra1, letra2; 
+        letra1 = s; 
+        letra2 = n; 
+        Scanner teclas = new Scanner(System.in); 
+        System.out.println("¿Quieres entrar a casa?" + "s/n");
+
+        s = teclas.next(); 
+        n = teclas.next(); 
+
+        do { 
+            
+        } while (condition);
+        
+
+
+
+
+
+
+
+    }
+}
